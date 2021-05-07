43a0b7745be4c81c62844146a974ab3bbbbbb351
Malyuta Aleksey <overby999@gmail.com>
2021-05-07 16:36:03 +0300

    revert

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+export declare class CaseConverter {
+    static converters: {
+        snakeToCamel: (object: object) => void;
+        camelToSnake: (object: object) => void;
+    };
+    private readonly converter;
+    constructor(converter: Function);
+    private parse;
+    convert<T>(data: T): Promise<T>;
+}
+export default CaseConverter;
